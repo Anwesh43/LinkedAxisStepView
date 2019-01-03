e835be85ffd76e82eeacded2bdc482c5476d6648
Anwesh43 <anweshthecool0@gmail.com>
2019-01-03 09:12:35 +0530

    adding libraries and defining constants

--- axisstepview/src/main/java/com/anwesh/uiprojects/axisstepview/AxisStepView.kt
+++ axisstepview/src/main/java/com/anwesh/uiprojects/axisstepview/AxisStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.axisstepview
 /**
  * Created by anweshmishra on 03/01/19.
  */
+
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val axises : Int = 2
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.8f
+val foreColor : Int = Color.parseColor("#43A047")
+val backColor : Int = Color.parseColor("#BDBDBD")
