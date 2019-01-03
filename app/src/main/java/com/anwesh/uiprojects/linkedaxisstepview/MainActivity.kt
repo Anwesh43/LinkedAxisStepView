5297692c0667c0dcc8185cdfc03067d710e15c21
Anwesh43 <anweshthecool0@gmail.com>
2019-01-03 09:52:57 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedaxisstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedaxisstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedaxisstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.axisstepview.AxisStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AxisStepView.create(this)
     }
 }
