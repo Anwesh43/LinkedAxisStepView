8a52053445eafc531772ebb5c1cbea13c0f1132c
Anwesh43 <anweshthecool0@gmail.com>
2019-01-03 09:31:28 +0530

    adding drawASNode canvas extension method

--- axisstepview/src/main/java/com/anwesh/uiprojects/axisstepview/AxisStepView.kt
+++ axisstepview/src/main/java/com/anwesh/uiprojects/axisstepview/AxisStepView.kt
@@ -27,3 +27,40 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float =  mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawASNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val sc11 : Float = sc1.divideScale(0, 2)
+    val sc12 : Float = sc1.divideScale(1, 2)
+    val arrowSize : Float = size / 5
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(gap * (i + 1), h / 2)
+    translate(-size, -size)
+    drawLine(0f, 0f, 2 * size * sc2, 2 * size * sc2, paint)
+    for (j in 0..(axises - 1)) {
+        val scj1 : Float = sc11.divideScale(j, axises)
+        val scj2 : Float = sc12.divideScale(j, axises)
+        save()
+        rotate(90f * j)
+        drawLine(0f, 0f, 0f, -2 * size * scj1, paint)
+        save()
+        translate(0f, -(2 * size - arrowSize))
+        for (j in 0..(axises - 1)) {
+            save()
+            rotate(45f * (1 - 2 * j) * scj2)
+            drawLine(0f, 0f, 0f, -arrowSize)
+            restore()
+        }
+        restore()
+        restore()
+    }
+    restore()
+}
