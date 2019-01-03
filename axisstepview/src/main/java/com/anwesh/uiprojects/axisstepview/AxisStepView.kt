3ec5bd2410adbf2f983de2fe60114c762b4cce4d
Anwesh43 <anweshthecool0@gmail.com>
2019-01-03 09:59:55 +0530

    adding drawing fixes

--- axisstepview/src/main/java/com/anwesh/uiprojects/axisstepview/AxisStepView.kt
+++ axisstepview/src/main/java/com/anwesh/uiprojects/axisstepview/AxisStepView.kt
@@ -31,35 +31,37 @@ fun Float.updateScale(dir : Float, a : Int, b : Int) : Float =  mirrorValue(a, b
 fun Canvas.drawASNode(i : Int, scale : Float, paint : Paint) {
     val w : Float = width.toFloat()
     val h : Float = height.toFloat()
-    val gap : Float = w / (nodes + 1)
+    val gap : Float = h / (nodes + 1)
     val size : Float = gap / sizeFactor
     val sc1 : Float = scale.divideScale(0, 2)
     val sc2 : Float = scale.divideScale(1, 2)
     val sc11 : Float = sc1.divideScale(0, 2)
     val sc12 : Float = sc1.divideScale(1, 2)
-    val arrowSize : Float = size / 5
+    val arrowSize : Float = size / 4
     paint.color = foreColor
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     paint.strokeCap = Paint.Cap.ROUND
     save()
-    translate(gap * (i + 1), h / 2)
+    translate(w / 2, gap * (i + 1))
     translate(-size, -size)
-    drawLine(0f, 0f, 2 * size * sc2, 2 * size * sc2, paint)
+    drawLine(0f, 0f, 1.75f * size * sc2, -1.75f * size * sc2, paint)
     for (j in 0..(axises - 1)) {
         val scj1 : Float = sc11.divideScale(j, axises)
         val scj2 : Float = sc12.divideScale(j, axises)
         save()
         rotate(90f * j)
         drawLine(0f, 0f, 0f, -2 * size * scj1, paint)
-        save()
-        translate(0f, -(2 * size - arrowSize))
-        for (j in 0..(axises - 1)) {
+        if (scj2 > 0) {
             save()
-            rotate(45f * (1 - 2 * j) * scj2)
-            drawLine(0f, 0f, 0f, -arrowSize, paint)
+            translate(0f, -2 * size)
+            for (j in 0..(axises - 1)) {
+                save()
+                rotate(60f * (1 - 2 * j) * scj2)
+                drawLine(0f, 0f, 0f, arrowSize, paint)
+                restore()
+            }
             restore()
         }
-        restore()
         restore()
     }
     restore()
@@ -224,7 +226,7 @@ class AxisStepView(ctx : Context) : View(ctx) {
         fun create(activity : Activity) : AxisStepView {
             val view : AxisStepView = AxisStepView(activity)
             activity.setContentView(view)
-            return view 
+            return view
         }
     }
 }
