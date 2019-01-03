ec0cf3578bfcaca93241a1221073b81da94e1651
Anwesh43 <anweshthecool0@gmail.com>
2019-01-03 09:43:40 +0530

    adding AxisStep class

--- axisstepview/src/main/java/com/anwesh/uiprojects/axisstepview/AxisStepView.kt
+++ axisstepview/src/main/java/com/anwesh/uiprojects/axisstepview/AxisStepView.kt
@@ -173,4 +173,27 @@ class AxisStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class AxisStep(var i : Int) {
+
+        private var curr : ASNode = ASNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
